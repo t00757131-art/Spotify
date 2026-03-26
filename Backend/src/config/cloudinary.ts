ab5e61b968e539f41d93f5e7223c9a4161f10106
Yogesh <nitya.upmanyu@gmail.com>
2026-03-26 21:18:52 +0530

    fix bug

--- Backend/src/config/cloudinary.ts
+++ Backend/src/config/cloudinary.ts
@@ -5,8 +5,6 @@ cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
-    secure:true,
-    secure_transport:true,  
 })
 
 export default cloudinary;
